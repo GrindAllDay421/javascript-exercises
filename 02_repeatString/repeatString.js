30d870c24fe9cd5d43fdf5af6c98082b2bc219fc
GrindAllDay421 <codiejaworsky@gmail.com>
2022-02-04 20:48:12 -0700

    finished first 2 js exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+  if(num < 0) {
+    return 'ERROR';
+  }
 
+  let repeatStr = '';
+  for(let i = 0; i < num; i++) {
+    repeatStr = repeatStr.concat(str);
+  }
+  return repeatStr;
 };
 
 // Do not edit below this line
