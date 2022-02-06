7da0243d7f07c22cd5424ff1c8dbfcb1d6c0cd89
GrindAllDay421 <codiejaworsky@gmail.com>
2022-02-05 17:34:01 -0700

    finished exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let strArr = string.split('');
+  let reversed = strArr.reverse();
+  return reversed.join('');
 };
 
 // Do not edit below this line
