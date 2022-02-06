2a1e26f8a88aa2259b46a6f6a7ec6e6554e5f051
GrindAllDay421 <codiejaworsky@gmail.com>
2022-02-05 17:51:24 -0700

    finished exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...target) {
+  let filteredArr = arr;
+  for(let i = 0; i < target.length; i++) {
+    filteredArr = filteredArr.filter(item => item !== target[i]);
+  }
+  return filteredArr;
 };
 
 // Do not edit below this line
