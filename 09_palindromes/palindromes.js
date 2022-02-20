a9003b321186841f249e432efcbbc8d926ebe72e
GrindAllDay421 <codiejaworsky@gmail.com>
2022-02-19 19:39:10 -0700

    finished palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const textTest = /[a-zA-Z]/g;
+  const lettersOnly = word.match(textTest)
+    .join('')
+    .toLowerCase();
+  const backwards = word.match(textTest)
+    .reverse()
+    .join('')
+    .toLowerCase();
+  return (lettersOnly === backwards ? true : false);
 };
 
 // Do not edit below this line
