46ed15491e6969ff7a3f4b391486d009b2966b7c
GrindAllDay421 <codiejaworsky@gmail.com>
2022-02-19 20:27:01 -0700

    finished fibonacci sequence exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  let fibArr = [1, 1];
+  let goalNum = num;
 
+  if(typeof goalNum !== 'number') {
+    goalNum = parseInt(num);
+  }
+
+  if(num < 0) {
+    return 'OOPS';
+  } else if(goalNum === 1 || goalNum === 2) {
+    return 1;
+  } else {
+    let nextNum = 0;
+
+    for(let sequence = 2; sequence < goalNum; sequence++) {
+      nextNum = fibArr[fibArr.length - 1] + fibArr[fibArr.length - 2];
+      fibArr.push(nextNum);
+    }
+    return nextNum;
+  }
 };
 
 // Do not edit below this line
