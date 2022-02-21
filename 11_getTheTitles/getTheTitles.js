85d02928fe2203cc98dd6a885900235c49204d15
GrindAllDay421 <codiejaworsky@gmail.com>
2022-02-21 02:23:12 -0700

    finished getTheTitle exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookList) {
+  let titleArr = bookList.map(book => book.title);
+  return titleArr;
 };
 
 // Do not edit below this line
